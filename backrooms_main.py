794ac2184dd528caeb6d573d37d22a8a5f248c0c
commissar-Alex <crestviewspirit247@gmail.com>
2022-03-22 15:49:11 -0400

    Second Update to Shell
    A second update including basic movement, camera offset, and speed modulation using shift/control.

--- backrooms_main.py
+++ backrooms_main.py
@@ -9,8 +9,12 @@ import pygame
 pygame.init()
 win_x, win_y = 800, 600
 win = pygame.display.set_mode((win_x, win_y))
-clock = pygame.time.Clock
+clock = pygame.time.Clock()
 running = True
+
+# Establishes baseline variables.
+move_speed = 100
+player_x, player_y = 0, 0
 
 ## MAIN GAMEPLAY LOOP ##
 while running :
@@ -29,12 +33,49 @@ while running :
 
         ## KEYBOARD ONE-PRESS INPUT ##
         if event.type == pygame.KEYDOWN :
-            if pygame.key == pygame.K_ESCAPE :
+            if event.key == pygame.K_ESCAPE :
                 running = False
 
-        ## CONTINUOUS KEYBOARD INPUT ##
-        keys = pygame.key.get_pressed()
+            # Speed control using shift/control.
+            if event.key == pygame.K_LSHIFT :
+                move_speed = 300
 
+            elif event.key == pygame.K_LCTRL :
+                move_speed = 25
+
+        ## KEYBOARD ONE-UP INPUT ##
+        if event.type == pygame.KEYUP :
+            # Resets move speed.
+            if pygame.key == pygame.K_LSHIFT or pygame.key == pygame.K_LCTRL :
+                move_speed = 100
+
+    ## CONTINUOUS KEYBOARD INPUT ##
+    keys = pygame.key.get_pressed()
+
+    # Basic WASD movement.
+    if keys[pygame.K_w] :
+        player_y -= move_speed * delta_time
+
+    if keys[pygame.K_s] :
+        player_y += move_speed * delta_time
+
+    if keys[pygame.K_d] :
+        player_x += move_speed * delta_time
+
+    if keys[pygame.K_a] :
+        player_x -= move_speed * delta_time
+
+    ## RENDERING ##
+    win.fill((0, 0, 0))
+
+    # Temp code to showcase movement.
+    pygame.draw.rect(win, (255, 0, 0), (0 - player_x, 0 - player_y, 10, 10), 1)
+
+    # Draws player.
+    pygame.draw.circle(win, (255, 255, 255), (win_x / 2, win_y / 2), 20)
+
+    # Finalizes render.
+    pygame.display.flip()
 
 # If running is set to False, runs this code.
 pygame.quit()
