5362df901184e8e74f8e87c92539b615c07ec1cb
Ech0Epsil0n <damioncsmith13@yahoo.com>
2022-03-22 15:51:54 -0400

    map maze layout

--- backrooms_main.py
+++ backrooms_main.py
@@ -1,3 +1,22 @@
+# Main file for running the Backrooms game
+
+import pygame
+running = True
+
 print("Welcome to the Backrooms")
+
+while running:
+    screen = pygame.display.set_mode((800, 600))
+    screen.fill((255, 255, 255))
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+        if event.type == pygame.QUIT:
+            running = False
+            
+    pygame.display.flip()
+pygame.quit()
 
 
