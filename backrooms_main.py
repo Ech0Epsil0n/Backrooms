d68b746b6e70aa7b8a613101223ceb2078e231c9
Ech0Epsil0n <damioncsmith13@yahoo.com>
2022-03-22 16:29:45 -0400

    Update backrooms_main.py

--- backrooms_main.py
+++ backrooms_main.py
@@ -1,22 +1,75 @@
-# Main file for running the Backrooms game
+## INTO THE BACKROOMS: MAIN FILE ##
+## CREATED ON MARCH 17TH, 2022 ##
+## ETGG 1802 ##
 
+## IMPORTS AND INITIALIZATIONS ##
 import pygame
+
+# Pygame standard initialization.
+pygame.init()
+win_x, win_y = 800, 600
+win = pygame.display.set_mode((win_x, win_y))
+clock = pygame.time.Clock
+clock = pygame.time.Clock()
 running = True
 
-print("Welcome to the Backrooms")
+# Establishes baseline variables.
+move_speed = 100
+player_x, player_y = 0, 0
 
-while running:
-    screen = pygame.display.set_mode((800, 600))
-    screen.fill((255, 255, 255))
+## MAIN GAMEPLAY LOOP ##
+while running :
+    # Time control.
+    delta_time = clock.tick() / 1000
 
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+    ## EVENT HANDLER ##
+    for event in pygame.event.get() :
+        ## KEYBOARD ONE-PRESS INPUT ##
+        if event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_ESCAPE :
                 running = False
-        if event.type == pygame.QUIT:
-            running = False
-            
-    pygame.display.flip()
-pygame.quit()
 
+            # Speed control using shift/control.
+            if event.key == pygame.K_LSHIFT :
+                move_speed = 300
+
+            elif event.key == pygame.K_LCTRL :
+                move_speed = 25
+
+        ## KEYBOARD ONE-UP INPUT ##
+        if event.type == pygame.KEYUP :
+            # Resets move speed.
+            if pygame.key == pygame.K_LSHIFT or pygame.key == pygame.K_LCTRL :
+                move_speed = 100
+
+    ## CONTINUOUS KEYBOARD INPUT ##
+    keys = pygame.key.get_pressed()
+
+    # Basic WASD movement.
+    if keys[pygame.K_w] :
+        player_y -= move_speed * delta_time
+
+    if keys[pygame.K_s] :
+        player_y += move_speed * delta_time
+
+    if keys[pygame.K_d] :
+        player_x += move_speed * delta_time
+
+    if keys[pygame.K_a] :
+        player_x -= move_speed * delta_time
+
+    ## RENDERING ##
+    win.fill((0, 0, 0))
+
+    # Temp code to showcase movement.
+    pygame.draw.rect(win, (255, 0, 0), (0 - player_x, 0 - player_y, 10, 10), 1)
+
+    # Draws player.
+    pygame.draw.circle(win, (255, 255, 255), (win_x / 2, win_y / 2), 20)
+
+    # Finalizes render.
+    pygame.display.flip()
+
+# If running is set to False, runs this code.
+pygame.quit()
 
