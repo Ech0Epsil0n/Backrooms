d9da94c4e0ef19b4bf532f1537bda48d066bef2d
Ech0Epsil0n <damioncsmith13@yahoo.com>
2022-04-20 13:18:49 -0400

    Update background test.py

--- background test.py
+++ background test.py
@@ -1,3 +1,33 @@
-Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+from tkinter import *
+
+root = Tk()
+root.geometry("800x600")
+root.title("backrooms test")
+root.iconbitmap("e:/PycharmProjects/Backrooms/Assets/Video/Main Menu")
+
+global our_images, count
+count = -1
+
+out_images = [PhotoImage(file = "e:/PycharmProjects/Backrooms/Assets/Video/Main Menu/main menu.png"),
+              PhotoImage(file = "e:/PycharmProjects/Backrooms/Assets/Video/Main Menu/main menu2.png")]
+
+# Create a canvas
+my_canvas = Canvas(root, width = 800, height = 600, highlightthickness = 0)
+my_canvas.pack(fill = "both", expand = True)
+
+# Set canvas image
+my_canvas.create_image(0, 0, image = out_images[0], anchor = "nw")
+
+def next():
+    global count
+    if count == 1:
+        my_canvas.create_image(0, 0, image = out_images[0], anchor = "nw")
+        count = 0
+    else:
+        my_canvas.create_image(0, 0, image = out_images[count + 1], anchor = "nw")
+        count += 1
+
+    root.after(500, next)
+
+next()
+mainloop()
